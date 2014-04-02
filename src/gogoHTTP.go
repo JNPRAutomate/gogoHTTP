d98b188e260f8f5178b3857089e115e024934be0
Rob Cameron <rwcameron@gmail.com>
2014-04-01 23:06:58 -0700

    added request count and ctrl-c handling

--- src/gogoHTTP.go
+++ src/gogoHTTP.go
@@ -1,29 +1,44 @@
-package gogoHTTP
+package main
 
 import (
    "strings"
    "log"
-    "flag"
-    "net/http"
+   "flag"
+   "net/http"
+   "os/signal"
+   "os"
 )
 
 var dirFlag = flag.String("d",".","Specify directory to serve (default: /.)")
 var portFlag = flag.String("p","8080","Specify directory to serve. (default: 8080)")
 
+var servecount int
+
 func Log(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        servecount = servecount + 1
         log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
         handler.ServeHTTP(w, r)
     })
 }
 
 func listen(){
+    //listen for ctrl-c
+    go func(){
+        sigchan := make(chan os.Signal, 1)
+        signal.Notify(sigchan, os.Interrupt)
+        <-sigchan
+        log.Printf("Served %d requests", servecount)
+        log.Println("Terminating gogoHTTP, Goodbye!")
+        os.Exit(0)
+    }()
     log.Printf("Listening on port %s",*portFlag)
     log.Fatal(http.ListenAndServe(strings.Join([]string{":",*portFlag},""), Log(http.FileServer(http.Dir(*dirFlag)))))
 }
 
 func main() {
     // Simple static webserver:
+    servecount = 0
     flag.Parse()
     listen()
 }
